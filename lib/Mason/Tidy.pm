928dcaf09149f32cc7a7685e3790621177b0983a
Jonathan Swartz <swartz@pobox.com>
2012-09-10 08:55:37 -0700

    remove all traces of html processing (archived to git/html-indent.git)

--- lib/Mason/Tidy.pm
+++ lib/Mason/Tidy.pm
@@ -1,6 +1,5 @@
 package Mason::Tidy;
 use File::Slurp;
-use HTML::PullParser;
 use IO::Scalar;
 use IPC::Run;
 use Method::Signatures::Simple;
@@ -12,22 +11,6 @@ my $marker_count  = 0;
 my $marker_prefix = '__masontidy__';
 my $open_block_regex =
   '<%(after|args|around|augment|before|class|doc|filter|flags|init|method|override|perl|shared|text)(\s+\w+)?>';
-
-# Implicitly empty tags (from emacs sgml-mode.el)
-#
-my @html_empty_tags = (
-    "area",  "base",    "basefont", "br",   "col",   "frame", "hr", "img",
-    "input", "isindex", "link",     "meta", "param", "wbr"
-);
-my %is_html_empty_tag = map { ( $_, 1 ) } @html_empty_tags;
-
-# Tags for which end tag is optional (from emacs sgml-mode.el)
-#
-my @html_unclosed_tags = (
-    "body", "colgroup", "dd", "dt",    "head", "html",  "li", "option",
-    "p",    "tbody",    "td", "tfoot", "th",   "thead", "tr"
-);
-my %is_html_unclosed_tag = map { ( $_, 1 ) } @html_unclosed_tags;
 
 method tidy ($source) {
     return $self->tidy_method($source);
@@ -53,7 +36,7 @@ method tidy_method ($source) {
             $cur_line = $end_line;
             next;
         }
-        $add_element->( 'html_line', $line );
+        $add_element->( 'text_line', $line );
     }
 
     # Create content from elements with non-perl lines as comments; perltidy;
@@ -79,27 +62,10 @@ method tidy_method ($source) {
             $add_element->( 'perl_line', "% " . $line );
         }
     }
-
-    # Create content from elements with perl lines as comments; indent html;
-    # reassemble list of elements from tidied html blocks and replaced elements
-    #
-    my $untidied_html = join( "\n",
-        map { $_->[0] eq 'html_line' ? $_->[1] : $self->replace_with_html_comment($_) } @elements );
-    my $tidied_html = $self->htmltidy($untidied_html);
-    @elements = ();
-    foreach my $line ( split( /\n/, $tidied_html ) ) {
-        if ( my $marker = $self->marker_in_line($line) ) {
-            $add_element->( @{ $self->restore($marker) } );
-        }
-        else {
-            $add_element->( 'html_line', $line );
-        }
-    }
+    my $final = join( "\n", map { $_->[1] } @elements );
 
     # Tidy Perl in <% %> tags
     #
-    my $final = join( "\n", map { $_->[1] } @elements );
-
     $final =~ s/<%\s+(.*?)\s+%>/'<% ' . $self->tidy_subst_expr($1) . ' %>'/ge;
 
     return $final;
@@ -139,10 +105,6 @@ method replace_with_perl_comment ($obj) {
     return "# " . $self->replace_with_marker($obj);
 }
 
-method replace_with_html_comment ($obj) {
-    return "<!-- " . $self->replace_with_marker($obj) . " -->";
-}
-
 method replace_with_marker ($obj) {
     my $marker = join( "_", $marker_prefix, $marker_count++ );
     $self->{markers}->{$marker} = $obj;
@@ -180,80 +142,6 @@ sub perltidy_postfilter {
     $buf =~ s/ *\{ *\{/ \{\{/g;
     $buf =~ s/ *\} *\}/\}\}/g;
     return $buf;
-}
-
-method jstidy ($source) {
-}
-
-method csstidy ($source) {
-}
-
-method htmltidy ($source) {
-    my $p = HTML::PullParser->new(
-        doc             => $source,
-        start           => '"S", text, tagname',
-        end             => '"E", text, tagname',
-        default         => '"O", text',
-        ignore_elements => [qw(script style)],
-    ) || die "Can't open: $!";
-    my $lineno = 0;
-    my ( @delta_before_line, @delta_after_line );
-
-    my @tagname_stack;
-    my $new_line = 1;
-    while ( my $token = $p->get_token ) {
-        my ( $type, $text, $tagname ) = @$token;
-        if ( $tagname eq 'script' ) {
-            $script_mode = ( $type eq 'S' ? 1 : 0 );
-        }
-        if (   $type eq 'S'
-            && $text !~ m{/>$}
-            && !$is_html_empty_tag{$tagname} )
-        {
-            $delta_after_line[$lineno]++;
-            if (   $is_html_unclosed_tag{$tagname}
-                && @tagname_stack
-                && $tagname_stack[-1] eq $tagname )
-            {
-                if ($new_line) {
-                    $delta_before_line[$lineno]--;
-                }
-                else {
-                    $delta_after_line[$lineno]--;
-                }
-            }
-            else {
-                push( @tagname_stack, $tagname );
-            }
-        }
-        if ( $type eq 'E' ) {
-            while ( my $popped_tagname = pop(@tagname_stack) ) {
-                if ($new_line) {
-                    $delta_before_line[$lineno]--;
-                }
-                else {
-                    $delta_after_line[$lineno]--;
-                }
-                last if $popped_tagname eq $tagname;
-            }
-        }
-        $lineno += ( $text =~ tr/\n// );
-        $new_line = ( $text =~ /\n\s*$/m );
-    }
-
-    my $level  = 0;
-    my $result = '';
-    my @lines  = split( "\n", $source );
-
-    for ( my $lineno = 0 ; $lineno < @lines ; $lineno++ ) {
-        $level += ( $delta_before_line[$lineno] || 0 );
-        $level = 0 if $level < 0;
-        my $line = $lines[$lineno];
-        $line =~ s/^\s+//m;
-        $result .= scalar( '  ' x $level ) . $line . "\n";
-        $level += ( $delta_after_line[$lineno] || 0 );
-    }
-    return $result;
 }
 
 sub trim {
