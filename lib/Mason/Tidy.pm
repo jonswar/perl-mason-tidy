2fdd499690f68b770bc71e3162a611c8286114f2
Jonathan Swartz <swartz@pobox.com>
2012-09-10 09:12:46 -0700

    Mooify

--- lib/Mason/Tidy.pm
+++ lib/Mason/Tidy.pm
@@ -3,23 +3,37 @@ use File::Slurp;
 use IO::Scalar;
 use IPC::Run;
 use Method::Signatures::Simple;
+use Moo;
 use Perl::Tidy qw();
 use strict;
 use warnings;
 
-my $marker_count  = 0;
-my $marker_prefix = '__masontidy__';
-my $open_block_regex =
-  '<%(after|args|around|augment|before|class|doc|filter|flags|init|method|override|perl|shared|text)(\s+\w+)?>';
+my $marker_count = 0;
+
+has '_open_block_regex' => ( is => 'lazy' );
+has 'marker_prefix' => ( is => 'ro', default => sub { '__masontidy__' } );
+has 'perltidy_argv' =>
+  ( is => 'ro', default => sub { '-noll -fnl -nbbc -i=2' } );
+
+method __build_open_block_regex () {
+    my $re = '<%(' . join( '|', @{ $self->block_names } ) . ')(\s+\w+)?>';
+    return qr/$re/;
+}
+
+method block_names () {
+    return
+      qw(after args around attr augment before class cleanup doc filter flags init method once override perl shared text);
+}
 
 method tidy ($source) {
     return $self->tidy_method($source);
 }
 
 method tidy_method ($source) {
-    my @lines       = split( /\n/, $source );
-    my @elements    = ();
-    my $add_element = sub { push( @elements, [@_] ) };
+    my @lines            = split( /\n/, $source );
+    my @elements         = ();
+    my $add_element      = sub { push( @elements, [@_] ) };
+    my $open_block_regex = $self->_open_block_regex;
 
     my $last_line = scalar(@lines) - 1;
     for ( my $cur_line = 0 ; $cur_line <= $last_line ; $cur_line++ ) {
@@ -27,10 +41,14 @@ method tidy_method ($source) {
         if ( $line =~ /^%%/ ) { $add_element->( 'ignore_line', $line ); next }
         if ( $line =~ /^%/ )  { $add_element->( 'perl_line',   $line ); next }
         if ( my ($block_type) = ( $line =~ $open_block_regex ) ) {
-            my $end_line = $self->capture_block( \@lines, $block_type, $cur_line + 1, $last_line );
-            my $block_contents = join( "\n", @lines[ $cur_line + 1 .. $end_line - 1 ] );
+            my $end_line =
+              $self->capture_block( \@lines, $block_type, $cur_line + 1,
+                $last_line );
+            my $block_contents =
+              join( "\n", @lines[ $cur_line + 1 .. $end_line - 1 ] );
             $block_contents = join( "\n",
-                $lines[$cur_line], $self->handle_block( $block_type, $block_contents ),
+                $lines[$cur_line],
+                $self->handle_block( $block_type, $block_contents ),
                 $lines[$end_line] );
             $add_element->( 'block', $block_contents );
             $cur_line = $end_line;
@@ -44,13 +62,16 @@ method tidy_method ($source) {
     #
     my $untidied_perl = join(
         "\n",
-        map { $_->[0] eq 'perl_line' ? substr( $_->[1], 2 ) : $self->replace_with_perl_comment($_) }
-          @elements
+        map {
+            $_->[0] eq 'perl_line'
+              ? substr( $_->[1], 2 )
+              : $self->replace_with_perl_comment($_)
+        } @elements
     );
     $self->perltidy(
         source      => \$untidied_perl,
         destination => \my $tidied_perl,
-        argv        => '-noll -fnl -nbbc -i=2'
+        argv        => $self->perltidy_argv,
     );
 
     @elements = ();
@@ -72,7 +93,11 @@ method tidy_method ($source) {
 }
 
 method tidy_subst_expr ($expr) {
-    $self->perltidy( source => \$expr, destination => \my $tidied_expr, argv => '-noll' );
+    $self->perltidy(
+        source      => \$expr,
+        destination => \my $tidied_expr,
+        argv        => '-noll'
+    );
     return trim($tidied_expr);
 }
 
@@ -94,7 +119,9 @@ method handle_block ($block_type, $block_contents) {
         );
         $block_contents = trim($tidied_block_contents);
     }
-    elsif ( $block_type =~ /^(after|augment|around|before|filter|method|override)/ ) {
+    elsif (
+        $block_type =~ /^(after|augment|around|before|filter|method|override)/ )
+    {
         $block_contents = $self->tidy_method($block_contents);
     }
     $block_contents =~ s/^(?!%)/  /mg;
@@ -106,12 +133,13 @@ method replace_with_perl_comment ($obj) {
 }
 
 method replace_with_marker ($obj) {
-    my $marker = join( "_", $marker_prefix, $marker_count++ );
+    my $marker = join( "_", $self->marker_prefix, $marker_count++ );
     $self->{markers}->{$marker} = $obj;
     return $marker;
 }
 
 method marker_in_line ($line) {
+    my $marker_prefix = $self->marker_prefix;
     if ( my ($marker) = ( $line =~ /(${marker_prefix}_\d+)/ ) ) {
         return $marker;
     }
